b9666ee81687718ba1315005b1eba2c1e90472eb
Anjal Afghan <anjal.afghan93@gmail.com>
2023-02-01 21:37:50 +0530

    NetWorth 1.0

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
+py_modules=[]
+
+setup(
+        name="netWorth",
+        version="0.0.1",
+        description="shows your current networth",
+        py_modules=[]
+    )
